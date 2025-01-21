d0f31aac582492d14644610f8063af06d638bf02
abhishek0948 <abhishekkaratagi325@gmail.com>
2025-01-21 11:30:35 +0530

    Deployment Config added

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -17,7 +17,7 @@ const NoChatSelected = () => {
         </div>
 
         {/* Welcome Text */}
-        <h2 className="text-2xl font-bold">Welcome to Tinder!</h2>
+        <h2 className="text-2xl font-bold">Welcome to SnapTalk!</h2>
         <p className="text-base-content/60">
           Select a conversation from the sidebar to start chatting
         </p>
